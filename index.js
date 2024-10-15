5b6e00f0f968a35c6fde7e2b210890b7ace5dcef
apnaranjith <ranjiphotos463@gmail.com>
2024-10-15 14:08:00 +0530

    changes added

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var express=require("express");
+var mysql2=require("mysql2");
 var app=express();
+var conn=mysql2.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Ra16nj08it01h@",
+    database:"9r",
+});
+conn.connect((err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("connected to mysql db");
+    }
+})
 app.post("/",(req,res)=>{
     res.send("hello");
 });
